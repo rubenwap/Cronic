b0874a9e86d42b15db5cf7124e1a792f2f00f9a6
Ruben Sanchez <rubensanchezgarcia@gmail.com>
2016-05-22 18:56:49 +0200

    Version preliminar css activo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,6 +20,29 @@
                       <h2>Cronic helps you to log your illness so you can have better control over it</h2>
                       <p class="lead">You cannot improve something unless you know what is wrong with it!</p>
                   </div>
+                  
+                  
+                  <div class = "intro">
+                  <i class="fa fa-pencil-square-o fa-5x" aria-hidden="true"></i>
+                       <p class="lead" > Keep track of your illness</p>
+                  </div>
+                  
+                      <div class = "intro">
+                  <i class="fa fa-user-md fa-5x" aria-hidden="true"></i>
+                    <p class="lead">Let a doctor review your entries</p>
+                  </div>
+                  
+                           <div class = "intro">
+                  <i class="fa fa-bell-o fa-5x" aria-hidden="true"></i>
+                    <p class="lead">Don´t miss any medical appointment or medicine schedule</p>
+                  </div>
+                  
+                        <div class = "intro">
+                  <p class="lead"><a href="{{ url('/register') }}">Sign up</a> today or <a href="{{ url('/login') }}">log in</a> if you already have an account</p>
+    </div>
+                  
+                  
+                  
               </div>
               <!-- /.row -->
           </div>
